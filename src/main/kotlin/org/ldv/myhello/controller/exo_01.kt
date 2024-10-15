b3520d15d353f6e5ea47e599c88299a48f17ced3
Inès <emmawasnotthere@gmail.com>
2024-10-15 11:59:41 +0200

    15/10/2024

--- src/main/kotlin/org/ldv/myhello/controller/exo_01.kt
+++ src/main/kotlin/org/ldv/myhello/controller/exo_01.kt
@@ -1,24 +1,29 @@
 package org.ldv.myhello.controller
 
 import org.springframework.ui.Model
+import org.springframework.ui.set
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.ResponseBody
 
 class exo_01 {
-    @GetMapping("/")
+    @GetMapping("/wonderfast")
     @ResponseBody
-    fun querryString (@RequestParam querry: Map<String, String>): String {
+    fun mycommand(model: Model, @RequestParam querry: Map<String, String>): String {
         val name = querry["name"] ?: ""
         val surname = querry["surname"] ?: ""
         val starter = querry["starter"] ?: ""
-        return "Hello ${name} ${surname} ${starter}!"
+        val maincourse = querry["maincourse"] ?: ""
+        val desert = querry["desert"] ?: ""
+        val drink = querry["drink"] ?: ""
     }
 
     @GetMapping("/")
-    fun index(model: Model): String {
-        model["monTitre"] = "Commande de ${}"
-        model[commande] = ""
+    fun index (model: Model): String {
+        model["monTitre"] = "Commande de ${name} ${surname}"
+        model["commande"] = "{$starter}, {$maincourse}, {$desert}, {$drink}"
         return "index"
     }
+
+
 }
